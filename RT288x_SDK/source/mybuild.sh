f00350be49ced9fe3272913867ee9a84cf0d8388
jimmyLin <jimmyLin@gmain.com>
2016-03-11 15:46:36 +0800

    ...

--- RT288x_SDK/source/mybuild.sh
+++ RT288x_SDK/source/mybuild.sh
@@ -4,32 +4,35 @@ if [ ! -d "lib" ]; then
 	tar zxvf lib.tgz
 	rm lib.tgz
 fi
-
 if [ ! -d "uClibc++-0.2.4" ]; then
 	tar zxvf uClibc++-0.2.4.tgz
 	rm uClibc++-0.2.4.tgz
 fi
-
 if [ ! -d "uClibc-0.9.33.2" ]; then
 	tar zxvf uClibc-0.9.33.2.tgz
 	rm uClibc-0.9.33.2.tgz
 fi
-
 if [ ! -d "uClibc++" ]; then
 	tar zxvf uClibc++.tgz
 	rm uClibc++.tgz
 fi
-
 if [ ! -f /usr/bin/flex ]; then
 	sudo apt-get install flex
 fi
+
+#---------------------------------------------------------#
 
 if [ "$1" = "1" ]; then
 	make menuconfig
 elif [ "$1" = "2" ]; then
 	make dep;make
+	ls -l images/
+	rm *_uImage
+elif [ "$1" = "3" ]; then
+	make distclean
 else
-        echo "Usage:mybuild.sh [1|2]"
+        echo "Usage:mybuild.sh [1|2|3]"
 	echo "./mybuild.sh 1 [make menuconfig]"
 	echo "./mybuild.sh 2 [make dep;make]"
+	echo "./mybuild.sh 3 [make distclean]"
 fi
